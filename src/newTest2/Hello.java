95c90c5c2b58f062966ed83d57f4b2582510d775
jmsoi <skhu202314153@gmail.com>
2025-09-10 15:26:39 +0900

    main.java 수정

--- src/newTest2/Hello.java
+++ src/newTest2/Hello.java
@@ -1,7 +1,14 @@
 package newTest2;
 
 public class Hello {
-	 public void say() {
-		 System.out.println("안녕하세요");
-	 }
+	String message;
+ 	public Hello() {
+ 		this("안녕하세요");
+ 	}
+ 	public Hello(String message) {
+ 		this.message = message;
+ 	}
+ 	public void say() {
+		System.out.println(message);
+ 	}
 }
